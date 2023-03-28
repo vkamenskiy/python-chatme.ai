cd4ecc680c9a4566df5c80c2e86e6baca078677f
Vladislav <djefry.kamen@gmail.com>
2023-03-28 22:31:33 +0400

    log telegram webhook data to telegram chat

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
+import os
 from http.server import BaseHTTPRequestHandler
+import requests
+
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
+    def do_POST(self):
+        content_length = int(
+            self.headers["Content-Length"]
+        )
+        post_data = self.rfile.read(content_length).decode("utf-8")
+        requests.post(
+            f"https://api.telegram.org/bot{os.environ.get('BOT_TOKEN')}/sendMessage",
+            {"text": post_data, "chat_id": int(os.environ.get('CHAT_ID'))},
+        )
+
+        self.send_response(200, message=None)
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
